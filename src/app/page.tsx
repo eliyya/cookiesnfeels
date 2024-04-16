f559b57db53ad1d23c1945228a212a6337f7823a
mael098 <juanvillegasmorales@outlook.com>
2024-04-15 18:22:23 -0600

    Update header and main section styles in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default async function Home() {
   return (
-    <main className="h-screen w-screen">
-      <header className="">
-        <nav className="flex ">
+    <main className="h-screen w-full">
+      <header className="h-16 w-full">
+        <nav className="fixed w-full flex">
           <div className="flex-1 bg-gray-800 text-white text-center p-4">
             <a href="/home">Home</a>
           </div>
@@ -18,11 +18,16 @@ export default async function Home() {
         </nav>
       </header>
       <section className="flex justify-center items-center h-full">
-        <main className="grid grid-col grid-cols-3 justify-center items-center  w-72 h-72">
-          <Image src="/4c34f0e70d69de7ca67c61cc6457d0b1.jpg" height={100} width={100} alt="imagen de comida" className="col-span-1 col-start-2"></Image>
-          <div className=" col-span-2 col-start-2 ">
+        <main className="w-72 h-72 grid  grid-rows-4 justify-center gap-2 items-center justify-items-center bg-violet-700 rounded-2xl  border-spacing-5 border-sky-900">
+          <Image src="/cσƒƒε☁☕.jpeg" height={60} width={90} alt="imagen de comida" className=" row-start-1 row-span-2   rounded-xl w-24 "></Image>
+          <div className="row-start-3 ">
             <h1>Recetas</h1>
             <p>Recetas de comida</p>
+          </div>
+          <div className="">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Ver recetas
+            </button>
           </div>
         </main>
       </section>
